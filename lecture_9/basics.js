de6feb8a73ba5810efc4a21c47ffa0516f3ad2a2
namanarora2707 <namanarora270704@gmail.com>
2026-06-11 18:10:12 +0530

    Notes

---
+++ lecture_9/basics.js
@@ -0,0 +1,33 @@
+var name="naman";
+var name="dinesh";
+console.log(name);
+let a="tulsi";
+a="rohan";
+console.log(a);
+const pi=3.1416159;
+console.log(pi)
+let x;
+console.log(x)
+let z=2**5;
+console.log(z)
+var b=10;
+var t="10";
+console.log(b==t);
+console.log(b===t);
+let r=5+"10";
+console.log(r);
+let s=5-"10";
+console.log(s);
+let m="10"*5;
+console.log(m)
+let student = {
+    name: "Naman",
+    age: 21
+};
+for(let key in student) {
+    console.log(key, student[key]);
+}
+function show(){
+    var q="aryan kumar";
+}
+console.log(q);
